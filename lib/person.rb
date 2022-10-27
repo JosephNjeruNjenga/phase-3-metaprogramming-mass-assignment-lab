6cff5f6d21a3ddd2d08cec48651b6898448a68af
+ <+>
2022-10-28 01:01:45 +0300

    Completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,5 @@
-require 'pry'
 class Person
   # your code here
-  # attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(args)
     args.each do |key, value|
@@ -10,4 +8,3 @@ class Person
     end
   end
 end
-binding.pry
